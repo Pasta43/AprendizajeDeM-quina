0f569d5837f8730edf5cbb044b8b359b30834246
Santiago Perez <santiago.perez01@correo.usa.edu.co>
2022-03-08 22:33:12 -0500

    Fixed predict_proba

--- first-term/exam/point1.py
+++ first-term/exam/point1.py
@@ -1,73 +1,97 @@
 from base_predict import Model
 
+
 class KNN(Model):
-    def __init__(self,k):
+    def __init__(self, k):
         """
         Constructor of KNN class
         k - that is the neighbor number
         """
         super().__init__()
-        self.neighbours=k
-        self.X_train=None
-        self.y=None
-    def fit(self,X,y):
+        self.neighbours = k
+        self.X_train = None
+        self.y = None
+        
+    def fit(self, X, y):
         """
         Fit the model with KNN algorithm
         X - features
         y - target
         """
-        self.X_train=X
-        self.y=y
-    def predict(self,X):
+        self.X_train = X
+        self.y = y
+        self.classes=self.get_unique_elements(self.y)
+
+    def predict(self, X):
         """
         Predict the labels of the data
         X - features
         """
-        prediction=[]
+        prediction = []
         for x in X:
-            distances=[]
+            distances = []
             for x_train in self.X_train:
-                distances.append((x_train,self.distance(x,x_train)))
-            distances.sort(key=lambda x:x[1])
-            k_nearest=distances[:self.neighbours]
-            k_nearest_labels=[]
+                distances.append((x_train, self.distance(x, x_train)))
+            distances.sort(key=lambda x: x[1])
+            k_nearest = distances[:self.neighbours]
+            k_nearest_labels = []
             for k in k_nearest:
                 k_nearest_labels.append(self.y[self.X_train.index(k[0])])
             prediction.append(self.majority(k_nearest_labels))
         return prediction
-    def predict_proba(self,X):
+
+    def predict_proba(self, X):
         """
-        Predict the probabilities of the data
+        Predict the probabilities of each class of the data
         X - features
         Returns the probabilities of the data (for each label)
-        """
+        # """
+        prediction = []
         for x in X:
-            distances=[]
+            distances = []
             for x_train in self.X_train:
-                distances.append(self.distance(x,x_train))
-            distances.sort()
-            k_nearest=distances[:self.neighbours]
-            k_nearest_labels=[]
+                distances.append((x_train, self.distance(x, x_train)))
+            distances.sort(key=lambda x: x[1])
+            k_nearest = distances[:self.neighbours]
+            k_nearest_labels = []
             for k in k_nearest:
-                k_nearest_labels.append(self.y[self.X_train[k]])
-            self.y.append(self.probability(k_nearest_labels))
-        return (self.y,[1-self.y[i] for i in range(len(self.y))])
-    def distance(self,x,x_train=None):
+                k_nearest_labels.append(self.y[self.X_train.index(k[0])])
+            prediction.append(self.probability(k_nearest_labels))
+        return prediction
+
+    def distance(self, x, x_train=None):
         """
         Calculate the distance between two data
         x - data
         x_train - data
         """
         return sum([(x[i]-x_train[i])**2 for i in range(len(x_train))])
-    def majority(self,k_nearest_labels):
+
+    def majority(self, k_nearest_labels):
         """
         Calculate the majority of the labels
         k_nearest_labels - labels of the k nearest data
         """
-        return max(set(k_nearest_labels),key=k_nearest_labels.count)
-    def probability(self,k_nearest_labels):
+        return max(set(k_nearest_labels), key=k_nearest_labels.count)
+
+    def probability(self, k_nearest_labels):
         """
-        Calculate the probabilities of the labels
+        Calculate the probabilities of the labels of each class
         k_nearest_labels - labels of the k nearest data
         """
-        return k_nearest_labels.count(self.majority(k_nearest_labels))/self.neighbours
+        probabilities = []
+        for label in self.classes:
+            probabilities.append(k_nearest_labels.count(label)/len(k_nearest_labels))
+        return probabilities
+        
+    def get_unique_elements(self, lst):
+        """
+        Get the unique elements of the list
+        lst - list
+        """
+        a = lst
+        b = []
+        for i in a:
+            if i not in b:
+                b.append(i)
+        return b
--- first-term/exam/test1.py
+++ first-term/exam/test1.py
@@ -5,7 +5,8 @@ x1 = np.array([2.3, 2.1, 3.7, 6.1, 6, 9.2, 11.0])
 x2 = np.array([1.3, 5.1, 0.7, 9.1, 4, 3.3, 5.0])
 X_train=[[x,y] for x,y in zip(x1,x2)]
 
-y = np.array([0, 1, 1, 0, 1, 0, 0])
+y = np.array(["no pasa", "pasa", "pasa", "no pasa", "pasa", "no pasa", "no pasa"])
 knn = KNN(3)
 knn.fit(X_train,y)
-print(knn.predict([[9,5]]))
+print(knn.predict([[9,5],[3,3]]))
+print(knn.predict_proba([[9,5],[3,3]]))
